8d9367df474380cf42b0cf8b47f7367548040c22
Carlos Alexandre <alexfilho062005@gmail.com>
2025-09-09 10:05:06 -0300

    :sparkles: feat: get course by name

--- src/main/java/com/carlosalexandredevv/crud_courses/modules/course/repositories/CourseRepository.java
+++ src/main/java/com/carlosalexandredevv/crud_courses/modules/course/repositories/CourseRepository.java
@@ -1,10 +1,12 @@
 package com.carlosalexandredevv.crud_courses.modules.course.repositories;
 
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.carlosalexandredevv.crud_courses.modules.course.entities.CourseEntity;
 
 public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
+    List<CourseEntity> findByNameContainingIgnoreCase(String name);
 }
